b07ff81594b17d9e0ef30444c26917509bd67a41
mohammedhu88 <mohammedhussin888@gmail.com>
2016-07-24 12:42:38 +0300

    new ver>>working
    haaaaaaaaaay

--- .myeclipse/phonegap_preview/ios/js/todo.js
+++ .myeclipse/phonegap_preview/ios/js/todo.js
@@ -1,36 +1,51 @@
-function getarray()
+function getArray()
 {
 	var toDoList = new Array;                        //Create An Array
-	var taskStr =localStorage.getItem(toDoList);     //get the task from input using LocalStorage
+	var taskStr =localStorage.getItem('toDoList');     //get the task from input using LocalStorage
 	if(taskStr !== null)                             //if the data is NOT empty --null-- 
          {
-		toDoList = JSON.parse(taskStr);
+		     toDoList = JSON.parse(taskStr);
          }
-	return toDOList; 	
+	return toDoList; 	
 }
 
-function addtask()
+function addTask()
 {
-	var task = document.getElementById("taskname").value;      //create a variable to hold value of input
+	var task = document.getElementById("taskName").value;      //create a variable to hold value of input
 	var array = getArray();                                    //create a variable to hold our array 
 	array.push(task);                                          //pushing the task into the array
-	localStorage.setItem(array, JSON.stringify(task));         //store the task LocalStorage saving to the array
+	localStorage.setItem('toDoList', JSON.stringify(array));         //store the task LocalStorage saving to the array
     show(); 
+    
     return false;
 }
-
+function removeTask()
+{
+	var id= this.getAttribute(id);                             // specific id for the clicked button
+	var array = getArray();                                    //create array
+	array.splice(id, 1) ;                                      //splicing the task we want to remove
+	localStorage.setItem('toDoList', JSON.stringify(array))    //saving new/edited array to localStorage
+	show();                                                    //show the latest list
+	return false;
+}
 function show()
 {
 	var array = getArray();                     //create variable to hold array
 	var htmlFormat = "<ul>";                    //add unordered list 
 	
-	for (var i=0 ; i < array.length; i++)       //a for loop to display the array
+	for (var i=0; i < array.length; i++)       //a for loop to display the array
 		{
-		htmlFomat += "<il>" + array[i] + "</il>";
+		     htmlFormat += "<li>" + array[i] + "<button class='remove'id='"+i+"'>X</button></li>";   //create a button tag using class and unique id
 		}
-	htmlFormat += "</ul>"
-	document.getElementById(array).innerHTML = htmlFormat;              //show the document 
+	htmlFormat += "</ul>";
+	document.getElementById('taskList').innerHTML = htmlFormat;              //show the document 
+            
+	var buttons = document.getElementsByClassName('remove');                 //create array for all buttons 
+	for(var j=0; j < buttons.length; j++)                                  // add an eventListiner for when every button is clicked
+	{
+		buttons[j].addEventListener('click', removeTask );
+	}
 }
 
-document.getElementById('add').addEventListener('click', addtask);
+document.getElementById('add').addEventListener('click', addTask);
 show();
